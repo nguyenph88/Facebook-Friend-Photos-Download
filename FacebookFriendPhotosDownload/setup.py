abdbea1c568a8f246c293e36115dbfa353a43d4e
Peter Nguyen <nguyenph882@gmail.com>
2020-02-18 09:32:55 -0800

    add setup file

---
+++ FacebookFriendPhotosDownload/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Facebook-Friend-Photos-Download",
+    version="0.0.1",
+    author="Peter Nguyen",
+    author_email="nguyenph882@gmail.com",
+    description="A simple tool to download all photos (from all albums) from your friend's facebook.",
+    long_description=long_description,
+    long_description_content_type="A simple tool to download all photos (from all albums) from your friend's facebook.",
+    url="https://github.com/nguyenph88/Facebook-Friend-Photos-Download",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
